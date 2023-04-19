e51cd7a8ee2b1c3724eaef00e4d9c3b8cd2751fb
localuser96 <localapostle@gmail.com>
2023-04-19 09:30:45 +0900

    VO 다시 추가

--- src/main/java/com/market/domain/AuctionVO.java
+++ src/main/java/com/market/domain/AuctionVO.java
@@ -1,5 +1,18 @@
 package com.market.domain;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class AuctionVO {
 
+	private int au_num;
+	private int au_startPrice;
+	private Date au_endTime;
+	private String au_sellerId;
+	private String au_buyerId;
+	private int au_bidPrice;
+	private int au_endPrice;
+	private String au_pic;
 }
