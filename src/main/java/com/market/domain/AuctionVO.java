20664d7b4d8a4eb8b8e1ebb7ed5ee5cdf44d5f5d
cjfals <cjfals7292@naver.com>
2023-04-19 17:48:01 +0900

    23.04.19

--- src/main/java/com/market/domain/AuctionVO.java
+++ src/main/java/com/market/domain/AuctionVO.java
@@ -1,5 +1,22 @@
 package com.market.domain;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class AuctionVO {
+	private int au_num;
+	private int au_startPrice;
+	private Date au_endTime;
+	private String sellerId;
+	private String buyerId;
+	private int au_bidPrice;
+	private int au_endPrice;
+	private String au_pic;
+	private int au_status;
+	private String au_title;
+	private String au_content;
+	private String au_category;
 
 }
